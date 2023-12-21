445acda5c953959e1f4fcfd2bb01aa63d4c317d3
gunjanak <janak.klal@gmail.com>
2023-12-21 23:27:21 +0545

    Order form auto filled if user is logged in

--- orders/forms.py
+++ orders/forms.py
@@ -6,3 +6,17 @@ class OrderCreateForm(forms.ModelForm):
         model = Order
         fields = ['first_name','last_name','email','address','postal_code','city']
         
+    def __init__(self,user=None,*args,**kwargs):
+        super(OrderCreateForm,self).__init__(*args,**kwargs)
+        if user and user.is_authenticated:
+       
+            print("**************")
+            print(user)
+            profile = user.profile
+            self.fields['first_name'].initial = profile.first_name
+            self.fields['last_name'].initial = profile.last_name
+            self.fields['email'].initial = profile.email
+            self.fields['address'].initial = profile.address
+            self.fields['postal_code'].initial = profile.postal_code
+            self.fields['city'].initial = profile.city
+
--- orders/views.py
+++ orders/views.py
@@ -9,9 +9,11 @@ from cart.cart import Cart
 def order_create_view(request):
     cart = Cart(request)
     if request.method == 'POST':
-        form = OrderCreateForm(request.POST)
+        form = OrderCreateForm(request.user,request.POST)
         if form.is_valid():
-            order = form.save()
+            order = form.save(commit=False)
+            order.save()
+
             for item in cart:
                 OrderItem.objects.create(order=order,
                                          product=item['product'],
@@ -27,7 +29,9 @@ def order_create_view(request):
         return render(request,'order_created.html',{"order":order})
     
     else:
-        form = OrderCreateForm()
+        print(request.user)
+        form = OrderCreateForm(user=request.user)
+      
 
 
     return render(request,'order_create.html',{'cart':cart,'form':form})
